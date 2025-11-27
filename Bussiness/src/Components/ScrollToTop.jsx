7cc91b14944a59998346c3d35f110e81f18654d9
Rythem aggarwal <rythemaggarwal7840@gmail.com>
2025-11-27 21:21:42 +0530

    update scroll again

--- Bussiness/src/Components/ScrollToTop.jsx
+++ Bussiness/src/Components/ScrollToTop.jsx
@@ -1,12 +1,18 @@
+// ScrollToTop.jsx
+// Save in Components folder - REPLACE OLD FILE
+
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation(); // 👈 search bhi add kiya
 
   useEffect(() => {
+    // Scroll to top
     window.scrollTo(0, 0);
-  }, [pathname]);
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  }, [pathname, search]); // 👈 search bhi dependency mein add kiya
 
   return null;
 };
